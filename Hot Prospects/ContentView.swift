c15a00db82df316b2a64d8856fead5246e6d55a0
Bkeane201 <bkeane117@gmail.com>
2021-06-05 17:33:25 -0700

    Initial Commit

---
+++ Hot Prospects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hot Prospects
+//
+//  Created by Brendan Keane on 2021-06-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
