a9e04ea2bdc7b3c1e116817cfa8220820089d854
Bkeane201 <bkeane117@gmail.com>
2021-06-13 09:07:45 -0700

    added ability to create and scan QR codes for use in our application

--- Hot Prospects/ProspectsView.swift
+++ Hot Prospects/ProspectsView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ProspectsView: View {
     @EnvironmentObject var prospects: Prospects
+    
+    @State private var isShowingScanner = false
     
     enum FilterType {
         case none, contacted, uncontacted
@@ -44,18 +47,48 @@ struct ProspectsView: View {
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .contextMenu {
+                        Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
+                            self.prospects.toggle(prospect)
+                        }
+                    }
                 }
             }
                 .navigationBarTitle(title)
                 .navigationBarItems(trailing: Button(action: {
+                    self.isShowingScanner = true
+                    /*
                     let prospect = Prospect()
                     prospect.name = "Paul Hudson"
                     prospect.emailAddress = "paul@hackingwithswift.com"
                     self.prospects.people.append(prospect)
+ */
                 }) {
                     Image(systemName: "qrcode.viewfinder")
-                    Text("Scane")
+                    Text("Scan")
                 })
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "paul Hudson\npaul@hackingwithswift.com", completion: self.handleScan)
+            }
+        }
+    }
+    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        self.isShowingScanner = false
+        switch result {
+        case .success(let code):
+            let details = code.components(separatedBy: "\n")
+            print(details)
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            
+            self.prospects.people.append(person)
+            
+        case .failure(let error):
+            print("Scanning failed")
         }
     }
 }
